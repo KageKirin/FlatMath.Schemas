7acfea3a58caf61cd06cdc66025b579780f2bee2
KageKirin <kagekirin+flatmath@gmail.com>
2025-01-09 22:11:57 +0900

    implemented dummy unit test

---
+++ Tests/Functionality/Dummy.cs
@@ -0,0 +1,13 @@
+using FlatMath.Schemas;
+using Xunit;
+
+namespace FlatMath.Schemas.Functionality.Tests;
+
+public class Dummy
+{
+    [Fact]
+    public void Test()
+    {
+        Assert.True(true);
+    }
+}
